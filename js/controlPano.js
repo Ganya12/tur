7b3290669a08bb148941405dfc780a066df62af2
Ganya12 <103940461+Ganya12@users.noreply.github.com>
2023-06-28 02:21:02 +0300

    Update controlPano.js

--- js/controlPano.js
+++ js/controlPano.js
@@ -6,23 +6,18 @@ const Panorama5 = new PANOLENS.ImagePanorama('assets/Panorama_5.jpg');
 let imageContainer = document.querySelector('.panorama__container');
 
 let SpotPositions = [
-    // Связь в коридоре
-    new THREE.Vector3(500, 0, 2000),
+    // Связь коридор и зал
+    new THREE.Vector3(0, 0, 1000),
     new THREE.Vector3(1630, 0, -660),
-    // Связь коридора_2 и комнаты переговоров
-    // new THREE.Vector3(1860, 0, 620),
-    // Связь коридора_1 и комнаты переговоров
+    // Связь зал и кухня
     new THREE.Vector3(1600, 0, 400),
     new THREE.Vector3(-1800, 0, 400),
-    // Связь переговорных
+    // Связь зал и спальня
     new THREE.Vector3(1600, 0, 0),
     new THREE.Vector3(1800, 0, 0),
-    // Связь коридора_1 и гостинной_1
+    // Связь спальня и ванная
     new THREE.Vector3(-800, 0, -2400),
     new THREE.Vector3(0, 0, 2300),
-    // Связь гостинной
-    // new THREE.Vector3(800, 0, 0),
-    // new THREE.Vector3(-800, 0, 0),
 ]
 
 const viewer = new PANOLENS.Viewer({
@@ -33,27 +28,20 @@ const viewer = new PANOLENS.Viewer({
     cameraFov: 100
 });
 
-// Связь в коридоре
+// Связь коридор и зал
 Panorama1.link(Panorama2, SpotPositions[0]);
 Panorama2.link(Panorama1, SpotPositions[1]);
 
-// // Связь коридора_2 и комнаты переговоров_1
-// Panorama2.link(Panorama3, SpotPositions[2]);
-
-// Связь коридора_1 и комнаты переговоров_1
+// Связь зал и кухня
 Panorama2.link(Panorama3, SpotPositions[2]);
 Panorama3.link(Panorama2, SpotPositions[3]);
 
-// Связь переговорной_1 и переговорной_2
+// Связь зал и спальня
 Panorama2.link(Panorama4, SpotPositions[4]);
 Panorama4.link(Panorama2, SpotPositions[5]);
 
-// Связь коридора_1 и гостинной_1
+// Связь спальня и ванная
 Panorama4.link(Panorama5, SpotPositions[6]);
 Panorama5.link(Panorama4, SpotPositions[7]);
-
-// // Связь гостинной_1 и гостинной_2
-// Panorama5.link(Panorama6, SpotPositions[8]);
-// Panorama6.link(Panorama5, SpotPositions[9]);
 
 viewer.add(Panorama1, Panorama2, Panorama3, Panorama4, Panorama5);
